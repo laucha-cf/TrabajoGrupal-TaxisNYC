c936301762fe411f74c85b787abb665ce6a9bc0c
laucha_cf <lautaropc2001@gmail.com>
2022-07-18 15:51:02 -0300

    Agrego Docstrings

--- week-2/Segmentacion_Tablas.py
+++ week-2/Segmentacion_Tablas.py
@@ -1,4 +1,4 @@
-# %%
+
 import pandas as pd
 import numpy as np
 
@@ -12,7 +12,6 @@ df = df_all.copy()
 # -- Cargamos los datos del clima para cada Borough --#
 df_weather = pd.read_csv('../data/NY_Boroughs_Weathers_01-2018.csv', parse_dates=['Datetime'])
 
-# %%
 # -- Agregamos info del clima al DataFrame de viajes --#
 bors = df_all_zones.Borough.unique()
 df_bors = pd.DataFrame(enumerate(bors), columns=['IdBorough', 'Borough'])
@@ -21,7 +20,7 @@ df_weather['Key_1'] = df_weather.Datetime.dt.strftime('%Y%m%d%H')
 df_weather.IdBorough = [*map(str, df_weather.IdBorough)]
 df_weather['Key_final'] = df_weather.IdBorough + df_weather.Key_1
 
-# %%
+
 df = df.merge(right=df_all_zones, how='left', left_on='PULocationID', right_on='LocationID')
 df = df.merge(right=df_bors, how='inner', on='Borough')
 df['Key_1'] = df['tpep_pickup_datetime'].dt.strftime('%Y%m%d%H')
@@ -32,13 +31,12 @@ columns = list(df_all) + ['Temperature', 'Precip_Type']
 df = df[columns]
 
 # -- Creación de tablas --#
-# %%
 # -- Vendor --#
 df_vendor = pd.DataFrame({
     'VendorID': [1, 2],
     'Vendor': ['Creative Mobile Technologies, LLC', 'VeriFone Inc.']
 })
-# %%
+
 # -- Borough --#
 boroughs = df_all_zones['Borough'].unique()
 
@@ -46,7 +44,7 @@ df_borough = pd.DataFrame({
     'Borough': boroughs
 })
 df_borough.insert(loc=0, column='BoroughID', value=np.arange(1, len(df_borough)+1))
-# %%
+
 # -- Service_Zone --#
 service_zones = df_all_zones['service_zone'].unique()
 
@@ -55,7 +53,7 @@ df_service_zone = pd.DataFrame({
 })
 df_service_zone.insert(loc=0, column='Service_ZoneID', value=np.arange(1, len(df_service_zone)+1))
 df_service_zone.loc[df_service_zone['Service_Zone'].isna(), 'Service_Zone'] = 'Unknown'
-# %%
+
 # -- Zone --#
 df_zone = df_all_zones[['LocationID', 'Zone', 'Borough', 'service_zone']]
 df_zone.loc[df_zone.LocationID == 265, 'Zone'] = 'Unknown'
@@ -65,12 +63,16 @@ df_zone.rename(columns={
 
 
 def replace_bor_for_index(p_x):
+    '''Retorna el índice correspondiente al Borough
+    param: p_x String ->Borough del cuál deseo saber el índice'''
     for j, bor in enumerate(df_borough['Borough']):
         if bor == p_x:
             return df_borough.iloc[j]['BoroughID']
 
 
 def replace_zone_for_index(p_x):
+    '''Retorna el índice correspondiente a la Service Zone
+    param: p_x String ->Service Zone de la cuál deseo saber el índice'''
     for j, zone in enumerate(df_service_zone['Service_Zone']):
         if zone == p_x:
             return df_service_zone.iloc[j]['Service_ZoneID']
@@ -78,25 +80,25 @@ def replace_zone_for_index(p_x):
 
 df_zone['BoroughID'] = df_zone['Borough'].apply(replace_bor_for_index)
 df_zone['Service_ZoneID'] = df_zone['service_zone'].apply(replace_zone_for_index)
-# %%
+
 # -- Rate_Code --#
 df_rate_code = pd.DataFrame({
     'RateCodeID': [*range(1, 7)],
     'RateCode': ['Standard rate', 'JFK', 'Newark', 'Nassau or Westchester', 'Negotiated fare', 'Group ride']
 })
-# %%
+
 # -- Payment_Type --#
 df_payment_type = pd.DataFrame({
     'PaymentTypeID': [*range(1, 7)],
     'PaymentType': ['Credit card', 'Cash', 'No charge', 'Dispute', 'Unknown', 'Voided trip']
 })
-# %%
+
 # -- Precip_Type --#
 df_precip_type = pd.DataFrame({
     'Precip_Type': ['No precipitacion', 'Rain', 'Snow', 'Rain and Snow']
 })
 df_precip_type.insert(loc=0, column='Precip_TypeID', value=np.arange(len(df_precip_type)))
-# %%
+
 # -- Calendar --#
 df['tpep_pickup_datetime'] = [*map(str, df['tpep_pickup_datetime'])]
 only_dates = df['tpep_pickup_datetime'].str.split()
@@ -138,23 +140,23 @@ df_trip = pd.DataFrame({
     'Temperature': df['Temperature'],
     'Precip_Type': df['Precip_Type']
 })
-# %%
+
 df_trip.Date = pd.to_datetime(df_trip.Date)
 df_trip.Date = df_trip.Date.dt.strftime('%Y%m%d')
 df_trip.Duration = df_trip.Duration.round()
 df_trip.Precip_Type = df_trip.Precip_Type.round()
 df_trip.insert(loc=0, column='IdTrip', value=np.arange(len(df_trip)))
-# %%
+
 # -- Payment --#
 df_payment = df[['RatecodeID', 'payment_type', 'fare_amount', 'extra', 'mta_tax',
                  'improvement_surcharge', 'tolls_amount', 'total_amount']]
 
 df_payment.insert(loc=0, column='IdTrip', value=df_trip['IdTrip'])
 df_payment.insert(loc=0, column='IdPayment', value=np.arange(len(df_payment)))
-# %%
+
 # -- Eliminamos columnas previamente utilizadas, pero que no cumplen con el modelo entidad-relación planteado --#
 df_zone = df_zone.drop(columns=['Borough', 'service_zone'])
-# %%
+
 # -- Renombrar Columnas - Normalizar --#
 df_vendor.columns = ['IdVendor', 'Vendor']
 df_calendar = df_calendar.rename(columns={'DateID': 'IdDate'})
@@ -170,7 +172,7 @@ df_payment = df_payment.rename(
     columns={'RatecodeID': 'IdRate_Code', 'payment_type': 'IdPayment_Type', 'fare_amount': 'Fare_Amount',
                            'extra': 'Extra', 'mta_tax': 'MTA_Tax', 'improvement_surcharge': 'Improvement_Surcharge',
                            'tolls_amount': 'Tolls_Amount', 'total_amount': 'Total_Amount'})
-# %%
+
 # -- Exportar a archivos CSV --#
 df_vendor.to_csv('../tables/vendor.csv', index=False)
 df_calendar.to_csv('../tables/calendar.csv', index=False)

50faa69772e39fee1f0d9371cd65fae944912cdf
jaimesarmientog <jaime.sarmientog@gmail.com>
2022-07-19 14:36:58 -0500

    Actualiza segmentacion para que almacene csv directo a S3, corrige el nombre del bucket en s3move

--- week-2/Segmentacion_Tablas.py
+++ week-2/Segmentacion_Tablas.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import numpy as np
+import boto3
 
 # -- Cargamos los datos de las zonas dentro de Nueva York --#
 df_all_zones = pd.read_csv('../data/taxi+_zone_lookup.csv')
@@ -184,3 +185,32 @@ df_rate_code.to_csv('../tables/rate_code.csv', index=False)
 df_payment_type.to_csv('../tables/payment_type.csv', index=False)
 df_zone.to_csv('../tables/zone.csv', index=False)
 df_payment.to_csv('../tables/payment.csv', index=False)
+
+# Se instancia la sesión con las credenciales de acceso para la conexión a S3 para carga de tablas al data lake.
+sesion = boto3.session.Session(
+    aws_access_key_id='AKIASPG3VE3KGTL2HGMN',
+    aws_secret_access_key='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',
+    region_name='us-east-1'
+)
+
+# Se establece la conexión al servicio
+connection = sesion.resource(service_name='s3')
+
+# Se asignan a una colección los objetos con prefijo raw_data, es decir todos los archivos
+# que se encuentran dentro de ese directorio, y el directorio como objeto, el directorio se ignora
+# los archivos que se encuentran dentro se copian al directorio processed_data/raw_data y luego se
+# borran del directorio raw_data original.
+
+buck = connection.Bucket('henry-pg9')
+files= buck.objects.filter(Prefix='processed/')
+for file in files:
+    if file.key[-1]=='/':
+        continue
+    else :
+        copy_source = {
+            'Bucket':'henry-pg9',
+            'Key': file.key
+        }
+        moved = buck.Object('processed_data/'+file.key)
+        moved.copy(copy_source)
+        file.delete()
--- week-2/s3move.py
+++ week-2/s3move.py
@@ -15,14 +15,14 @@ connection = sesion.resource(service_name='s3')
 # los archivos que se encuentran dentro se copian al directorio processed_data/raw_data y luego se
 # borran del directorio raw_data original.
 
-buck = connection.Bucket('copymovetest')
+buck = connection.Bucket('henry-pg9')
 files= buck.objects.filter(Prefix='raw_data/')
 for file in files:
     if file.key[-1]=='/':
         continue
     else :
         copy_source = {
-            'Bucket':'copymovetest',
+            'Bucket':'henry-pg9',
             'Key': file.key
         }
         moved = buck.Object('processed_data/'+file.key)

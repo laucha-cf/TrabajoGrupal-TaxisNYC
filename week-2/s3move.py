085fd1a6e695395b2da244a6512a9de570260a6a
jaimesarmientog <jaime.sarmientog@gmail.com>
2022-07-19 10:17:33 -0500

    Se agrega script para mover archivos en S3

---
+++ week-2/s3move.py
@@ -0,0 +1,30 @@
+import boto3
+
+# Se instancia la sesión con las credenciales de acceso para la conexión.
+sesion = boto3.session.Session(
+    aws_access_key_id='AKIASPG3VE3KGTL2HGMN',
+    aws_secret_access_key='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',
+    region_name='us-east-1'
+)
+
+# Se establece la conexión al servicio
+connection = sesion.resource(service_name='s3')
+
+# Se asignan a una colección los objetos con prefijo raw_data, es decir todos los archivos
+# que se encuentran dentro de ese directorio, y el directorio como objeto, el directorio se ignora
+# los archivos que se encuentran dentro se copian al directorio processed_data/raw_data y luego se
+# borran del directorio raw_data original.
+
+buck = connection.Bucket('copymovetest')
+files= buck.objects.filter(Prefix='raw_data/')
+for file in files:
+    if file.key[-1]=='/':
+        continue
+    else :
+        copy_source = {
+            'Bucket':'copymovetest',
+            'Key': file.key
+        }
+        moved = buck.Object('processed_data/'+file.key)
+        moved.copy(copy_source)
+        file.delete()
